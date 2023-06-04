d40d00e66a95348d3f40907a08f2c89decec3455
zozi macbook <y.700@live.com>
2023-06-04 19:36:36 +0400

    lol

---
+++ Client.cpp
@@ -0,0 +1,106 @@
+#include "Client.hpp"
+
+// Default const
+Client::Client()
+{}
+
+// cpy const
+Client::Client(const Client &src)
+{
+	fd = src.fd;
+	port = src.port;
+	nick = src.nick;
+	uname = src.uname;
+	name = src.name;
+	host = src.host;
+	state = src.state;
+}
+
+//parametirized const
+Client::Client(int fd, int port, const std::string &host)
+	: fd(fd), port(port), host(host)
+{
+	state = 0; // handhshake state
+}
+
+// destructor
+Client::~Client()
+{}
+
+// SETTERS
+void Client::set_state(int state)
+{
+	this->state = state;
+}
+
+void Client::set_nick(const std::string &nick)
+{
+	this->nick = nick;
+}
+
+void Client::set_uname(const std::string &uname)
+{
+	this->uname = uname;
+}
+
+void Client::set_name(const std::string &name)
+{
+	this->name = name;
+}
+
+// GETTERS
+int Client::get_fd() const
+{
+	return fd;
+}
+
+int Client::get_port() const
+{
+	return port;
+}
+
+int Client::get_state()
+{
+	return state;
+}
+
+std::string Client::get_nick() const
+{
+	return nick;
+}
+
+std::string Client::get_uname() const
+{
+	return uname;
+}
+
+std::string Client::get_name() const
+{
+	return name;
+}
+
+std::string Client::get_host() const
+{
+	return host;
+}
+
+// send msd
+void Client::send(const std::string &msg) const
+{
+	//implement send
+}
+
+// Reply to the client
+void Client::reply(const std::string &msg)
+{
+	// implement reply
+}
+
+// irc welcome msg
+void Client::welcome()
+{
+	// if _state is not 1 or uname is empty or name is empty or nick is empty:
+    //     return
+
+    // welcome msg
+}
--- Client.hpp
+++ Client.hpp
@@ -6,7 +6,7 @@
 /*   By: alalmazr <alalmazr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 19:10:46 by alalmazr          #+#    #+#             */
-/*   Updated: 2023/06/03 19:36:35 by alalmazr         ###   ########.fr       */
+/*   Updated: 2023/06/04 19:25:19 by alalmazr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,50 @@
 
 class Client
 {
-	private:
-		int fd;
-		int port;
-		std::string nick;
-		std::string uname;
-		std::string name;
-		std::string host;
-		//add smth for the channel
-		//0handshake 1login 2registed -1disconnected
-		int state;
+private:
+	int fd;
+	int port;
+	std::string nick;
+	std::string uname;
+	std::string name;
+	std::string host;
+	// add smth for the channel
+	// 0handshake 1login 2registed -1disconnected
+	int state;
 
-		//constructor
-		Client();
-		//cpy constructor
-		Client(const Client &src);
+	// constructor
+	Client();
+	// cpy constructor
+	Client(const Client &src);
 
-	public:
-		Client(int fd, int port, const std::string &host);
-		~Client();
+public:
+	Client(int fd, int port, const std::string &host);
+	~Client();
 
-		//setters
-		//getters
-		//check state
-		//send recieve actions
-		//void send(const string message) const;
-		//void reply(const string reply);
-		//welcome msg as in irc
+	// setters
+	void set_state(int state);
+	void set_nick(const std::string &nick);
+	void set_uname(const std::string &uname);
+	void set_name(const std::string &name);
+	// getters
+	int get_fd() const;
+	int get_port() const;
+	int get_state();
 
-		//client actions; join/leave
+	std::string get_nick() const;
+	std::string get_uname() const;
+	std::string get_name() const;
+	std::string get_host() const;
+
+	// check state
+	// send recieve actions
+	void send(const std::string &msg) const;
+	void reply(const std::string &msg);
+	// welcome msg as in irc
+	void welcome();
+	// client actions; join/leave
+	// int	join_channel(channel num);
+	// int leave_channel(channel num);
 };
 
 #endif
